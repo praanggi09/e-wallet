d28bc1b48b7435f5339a44faed0e7fc5537dcb1c
Anggi Pratama Nasution <anggi.nasution@packet-systems.com>
2023-08-22 21:27:16 +0700

    initial commit

---
+++ lib/ui/pages/sign_in.dart
@@ -0,0 +1,34 @@
+import 'package:e_wallet/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          Container(
+            width: 155,
+            height: 50,
+            margin: EdgeInsets.only(
+              top: 100,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/img_logo_light.png',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
